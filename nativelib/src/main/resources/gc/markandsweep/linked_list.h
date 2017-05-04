63760065c380f0e85eee5fabba6be9c1ef3d88fb
Denys Shabalin <denys.shabalin@epfl.ch>
2017-05-04 22:24:26 +0200

    Mov code of stack and linked list to headers

--- nativelib/src/main/resources/gc/markandsweep/linked_list.h
+++ nativelib/src/main/resources/gc/markandsweep/linked_list.h
@@ -7,16 +7,39 @@
 #include "bitmap.h"
 
 /**
- *
  * Allocates a new linked_list struct and set all fields to NULL
  */
-LinkedList *linked_list_alloc();
+inline LinkedList *linked_list_alloc() {
+    LinkedList *list = malloc(sizeof(LinkedList));
+    list->first = NULL;
+    list->last = NULL;
+    return list;
+}
 
 /**
  * Adds a block to the end of the list, does not merge blocks.
  * Sets the size to the given size and the tag to `tag_free`.
  */
-void linked_list_add_block(LinkedList *list, Block *block, size_t block_size);
+inline void linked_list_add_block(LinkedList *list, Block *block,
+                                  size_t block_size_with_header) {
+    // Set the header fields
+    block->header.size = block_size_with_header;
+    block->header.tag = tag_free;
+
+    assert(block_size_with_header >= 2);
+
+    // If the list is empty, set the block as first block
+    if (list->first == NULL) {
+        list->first = block;
+    } else {
+        // Otherwise the block is the next of the last block
+        list->last->next = block;
+    }
+    // Because the block is added at the end, there is no next and last is the
+    // added block
+    block->next = LIST_END;
+    list->last = block;
+}
 
 /**
  * Removes the block `block` from the linked list.
@@ -25,6 +48,24 @@ void linked_list_add_block(LinkedList *list, Block *block, size_t block_size);
  * If previous is NULL, block is expected to be the first block of the
  * linked_list.
  */
-void linked_list_remove_block(LinkedList *list, Block *block, size_t size);
+inline void linked_list_remove_block(LinkedList *list, Block *block,
+                                     size_t block_size_with_header) {
+    assert(block != NULL);
+
+    Block *next = block->next;
+
+    // If the block is the first element of the list
+    list->first = next;
+
+    // If the block to remove was the last block of the linked_list, update
+    // last.
+    if (list->last == block) {
+        assert(next == LIST_END);
+        list->last = NULL;
+    }
+
+    block->header.size = block_size_with_header;
+    block->header.tag = tag_allocated;
+}
 
 #endif // LINKED_LIST_H
--- nativelib/src/main/resources/gc/markandsweep/stack.h
+++ nativelib/src/main/resources/gc/markandsweep/stack.h
@@ -6,14 +6,37 @@
 #include <stdio.h>
 #include "types.h"
 
-Stack *stack_alloc(size_t size);
+inline Stack *stack_alloc(size_t size) {
+    assert(size % sizeof(Stack_Type) == 0);
+    Stack *stack = malloc(sizeof(Stack));
+    stack->current = 0;
+    stack->bottom = malloc(size);
+    stack->nb_words = size / sizeof(Stack_Type);
+    return stack;
+}
 
-int stack_push(Stack *stack, Stack_Type word);
+inline int stack_push(Stack *stack, Stack_Type word) {
+    if (stack->current < stack->nb_words) {
+        stack->bottom[stack->current++] = word;
+        return 0;
+    } else {
+        return 1;
+    }
+}
 
-Stack_Type stack_pop(Stack *stack);
+inline Stack_Type stack_pop(Stack *stack) {
+    assert(stack->current > 0);
+    return stack->bottom[--stack->current];
+}
 
-int stack_is_empty(Stack *stack);
+inline int stack_is_empty(Stack *stack) { return stack->current == 0; }
 
-void stack_double_size(Stack *stack);
+inline void stack_double_size(Stack *stack) {
+    assert(stack->current == 0);
+    int nb_words = stack->nb_words * 2;
+    stack->nb_words = nb_words;
+    free(stack->bottom);
+    stack->bottom = malloc(nb_words * sizeof(Stack_Type));
+}
 
 #endif // STACK_H
