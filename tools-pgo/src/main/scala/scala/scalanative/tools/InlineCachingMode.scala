024e79ccc227e0a052ee12c046768076db5c6028
Denys Shabalin <denys.shabalin@epfl.ch>
2019-04-24 15:23:26 +0200

    wip

--- tools-pgo/src/main/scala/scala/scalanative/tools/InlineCachingMode.scala
+++
@@ -1,9 +0,0 @@
-package scala.scalanative
-package tools
-
-sealed abstract class InlineCachingMode
-final case object NoInlineCaching                 extends InlineCachingMode
-final case object InlineCacheAll                  extends InlineCachingMode
-final case class InlineCacheN(n: Int)             extends InlineCachingMode
-final case class InlineCacheP(p: Double)          extends InlineCachingMode
-final case class InlineCacheNP(n: Int, p: Double) extends InlineCachingMode
--- tools-pgo/src/main/scala/scala/scalanative/tools/ProfileMode.scala
+++
@@ -1,7 +0,0 @@
-package scala.scalanative
-package tools
-
-sealed abstract class ProfileMode
-final case object NoProfile                         extends ProfileMode
-final case class CollectProfile(file: java.io.File) extends ProfileMode
-final case class UseProfile(file: java.io.File)     extends ProfileMode
